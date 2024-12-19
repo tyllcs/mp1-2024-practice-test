9e1fad699ad8090f97b92c53e06a0441735019a6
tyllcs <dashakras369@gmail.com>
2024-12-19 10:39:40 +0300

    Update task1.c

--- 3824Б1ПР1/12 Красильникова Дарья Юрьевна/task1.c
+++ 3824Б1ПР1/12 Красильникова Дарья Юрьевна/task1.c
@@ -3,5 +3,16 @@
 // Вернуть 0, если требуемые элементы отсутствуют.
 int task1(unsigned A[], size_t n, unsigned a, unsigned b)
 {
-  return -1;
+    int max_elem = -1;
+    for (size_t i = 0; i < n; i++) {
+        if ((a < A[i]) && (A[i] < b)) {
+            if (max_elem < A[i]) {
+                max_elem = A[i];
+            }
+        }
+    }
+    if (max_elem == -1) {
+        return 0;
+    }
+    return max_elem;
 }
