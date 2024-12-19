d5b6bf3d6b996eef80c392d467198ef798be7453
tyllcs <dashakras369@gmail.com>
2024-12-19 10:40:34 +0300

    Update task3.c

--- 3824Б1ПР1/12 Красильникова Дарья Юрьевна/task3.c
+++ 3824Б1ПР1/12 Красильникова Дарья Юрьевна/task3.c
@@ -1,7 +1,25 @@
 // Задача 3. Даны: символ ch и строка s. 
 // Сформировать новую строку, в которой все вхождения символа ch в строку s
 // удвоены. Пример: ch = ‘g’, s = “abgcdgefgk”, ответ: “abggcdggefggk”.
-char* task3(char *s, char ch)
-{
-  return "task3";
+#include <malloc.h>
+char* task3(const char* s, char ch) {
+    int len = strlen(s);
+    int cnt_ch_in_s = 0;
+    for (int i = 0; i < len; i++) {
+        if (s[i] == ch)
+            cnt_ch_in_s++;
+    }
+
+    char* answer = (char*)malloc((len + cnt_ch_in_s + 1) * sizeof(char));
+
+    int j = 0;
+    for (int i = 0; i < len; i++) {
+        answer[j++] = s[i];
+        if (s[i] == ch) {
+            answer[j++] = ch;
+        }
+    }
+    answer[j] = '\0';
+
+    return answer;
 }
