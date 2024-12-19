a8d49de72a4c13897b20226fb84d99eb56cad168
tyllcs <dashakras369@gmail.com>
2024-12-19 10:07:10 +0300

    Update task1.c

--- 3824Б1ПР1/12 Красильникова Дарья Юрьевна/task1.c
+++ 3824Б1ПР1/12 Красильникова Дарья Юрьевна/task1.c
@@ -1,7 +1,17 @@
 // Задача 1. Даны числа a, b (0 < a < b) и непустой массив A из N элементов.
 // Найти максимальный из тех элементов A, которые содержатся в интервале (a, b).
 // Вернуть 0, если требуемые элементы отсутствуют.
+#include <stdio.h>
+
 int task1(unsigned A[], size_t n, unsigned a, unsigned b)
 {
-  return -1;
+  int max_elem = -1;
+  for (size_t i = 0; i < n; i++) 
+    if ((a < A[i]) && (A[i] < b))
+       if (max_elem < A[i])
+         max_elem = A[i];
+  
+  if (max_elem == -1) 
+    return 0;
+  return max_elem;
 }
